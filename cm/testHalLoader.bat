6fb187e5c697c5359d809d4d5e668ced5dd399b6
Jon Zlotnik <j.zlotnik@live.ca>
2020-12-06 21:02:45 -0500

    trying to trick git into merging the branches

--- cm/testHalLoader.bat
+++
@@ -1,6 +0,0 @@
-avr-gcc -Os -Wall -DF_CPU=16000000UL -mmcu=atmega328p  _console.c _xtoa.c hal_loader.c hal.c out.c vm.c vmstack.c bsl_uart.c bsl_COut.c  -o hal_loader.o
-avr-objcopy -O ihex -j .text -j .data hal_loader.o  hal_loader.hex
-avrdude -c arduino -p atmega328p -b 57600 -P COM5 -D -Uflash:w:hal_loader.hex:i
-echo done.  press any key to exit script
-
-pause

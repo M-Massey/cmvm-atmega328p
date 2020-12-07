0210c5764e61cbb3ded11d2dc452958cc982a052
Jon Zlotnik <j.zlotnik@live.ca>
2020-12-06 21:40:55 -0500

    brought projects to root of repo

---
+++ loader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

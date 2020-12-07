b020417539dc4e8b38b0db8fbd322c0972c2f261
Jon Zlotnik <j.zlotnik@live.ca>
2020-12-06 23:59:17 -0500

    removed semicolon

--- loader/Program.cs
+++ loader/Program.cs
@@ -165,7 +165,7 @@ public class SerialComPort {
         // Create a new SerialPort with the same Arduino Nano settings.
         _serialPort = new SerialPort();
         _serialPort.PortName = "COM5";
-        _serialPort.BaudRate = 57600; ;
+        _serialPort.BaudRate = 57600;
         _serialPort.Parity = Parity.None;
         _serialPort.DataBits = 8;
         _serialPort.StopBits = StopBits.One;

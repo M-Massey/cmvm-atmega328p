77f703502e4d882bd3413ee0f3d816af33f5e986
Jon Zlotnik <j.zlotnik@live.ca>
2020-12-07 15:55:16 -0500

    disabled interrupts during io register operations

--- vm/_avr_ioreg.c
+++ vm/_avr_ioreg.c
@@ -5,6 +5,7 @@
 */
 
 #include "_avr_ioreg.h"
+#include "_avr_interrupts.h"
 
 #include <avr/io.h>
 
@@ -15,11 +16,18 @@ static u32 _nReads;
 static u32 _nWrites;
 
 u32 bsl_IOReg_Read(u32 port) {
+    u16 temp_flags = __saveAndDisable();
     ++_nReads;
-    return _SFR_IO8((u8)port);
+    u32 out = _SFR_IO8((u8)port);
+    __restore(temp_flags);
+    __sei();
+    return out;
 }
 
 void bsl_IOReg_Write(u32 port, u32 value) {
+    u16 temp_flags = __saveAndDisable();
     ++_nWrites;
     _SFR_IO8((u8)port) = (u8)value;
+    __restore(temp_flags);
+    __sei();
 }
